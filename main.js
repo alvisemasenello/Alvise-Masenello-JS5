3c5fe89ae7db57643f5d81fac276f8c3548aa976
Alvise Masenello <alvisemasenello@me.com>
2023-03-29 16:15:33 +0200

    primo commit

---
+++ main.js
@@ -0,0 +1,33 @@
+// ESERCIZIO 1
+
+// arr1 = [3, 7, -2, 5, 8, 1, 2, 5, 6, -4];
+
+// arr1.sort(function (a, b){
+//     return a - b;
+// })
+
+// console.log(arr1);
+
+// arr1.sort(function (a, b){
+//     return b - a;
+// })
+
+// console.log(arr1);
+
+
+// ESERCIZIO 2
+
+// arr1 = [3, 7, -2, 5, 8, 1, 2, 5, 6, -4];
+
+// let arrmax = console.log(Math.max(...arr1));
+
+// let arrmin = console.log(Min.math(...arr1));
+
+
+// ESERCIZIO 3
+
+// x = [3, 7, 12, 'Paolo', 11];
+
+// y = 3;
+
+// console.log(x.includes(y, 0));
